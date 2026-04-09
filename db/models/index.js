497749e1f6f749a354001596f83c250892706c57
Valeriy <epifanovvaleriy@gmail.com>
2026-04-09 15:59:20 +0300

    last norm

--- db/models/index.js
+++ db/models/index.js
@@ -50,7 +50,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.js')[env];
+const config = require('../config/config')[env];
 
 const db = {};
 
