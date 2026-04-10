85942caf691df2757da5a5721af5833ec59bdfa6
Valeriy <epifanovvaleriy@gmail.com>
2026-04-10 16:08:00 +0300

    wrager

--- 11routes/11strongholdRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const strongholdController = require('../11controllers/strongholdController');
-
-router.get('/defenses', strongholdController.getGlobalDefenses);
-router.put('/defenses', strongholdController.saveGlobalDefenses);
-
-module.exports = router;
